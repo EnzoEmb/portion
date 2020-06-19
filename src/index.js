80076d6ad2d70617ba735def940725d92f51291d
Enzo Vergara <enzoamb123@gmail.com>
2020-06-19 20:14:55 -0300

    cleaning

--- src/index.js
+++ src/index.js
@@ -1,26 +1,13 @@
 const CONTAINER = document.getElementById("content")
 const ELEMENT = document.querySelector(".element")
 
-
-
 // cuando presiona un boton en el elemento con foco
 document.activeElement.addEventListener("keypress", function (e) {
   // si el boton es enter
   if (e.keyCode == 13) {
     // añadimos nuevo parrafo
     createElement(e);
-    // CONTAINER.insertAdjacentHTML('beforeend', '<div class="element" draggable="true"><div class="element-content" contenteditable="true"></div></div>');
-    // document.querySelector('.element:last-child').focus();
-    // setTimeout(function(){
-      
-    // CONTAINER.lastChild.focus()
-    // }, 100)
-    // e.target.insertAdjacentHTML('afterend', '<div class="element" contenteditable="true"></div>');
-    // setTimeout(function(){CONTAINER.lastChild.focus()}, 500);
-
   }
-
-
 });
 
 
@@ -30,10 +17,6 @@ function createElement(e) {
   var PARENT = e.target.parentElement;
   PARENT.insertAdjacentHTML('afterend', '<div class="element" draggable="true"><div class="element-content" contenteditable="true"></div></div>');
   PARENT.nextSibling.getElementsByClassName('element-content')[0].focus()
-
-
-  // CONTAINER.insertAdjacentHTML('beforeend', '<div class="element" draggable="true"><div class="element-content" contenteditable="true"></div></div>');
-  // CONTAINER.lastChild.getElementsByClassName('element-content')[0].focus();
 
 }
 
