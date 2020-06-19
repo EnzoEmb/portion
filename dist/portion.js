7094b77faa5aaac7c7f778015cc4ff3cd8bc353a
Enzo Vergara <enzoamb123@gmail.com>
2020-06-19 20:04:10 -0300

    create elements and testing sortable

--- dist/portion.js
+++ dist/portion.js
@@ -8,13 +8,13 @@ document.activeElement.addEventListener("keypress", function (e) {
   // si el boton es enter
   if (e.keyCode == 13) {
     // añadimos nuevo parrafo
-    // createElement();
-    CONTAINER.insertAdjacentHTML('beforeend', '<div class="element" draggable="true"><div class="element-content" contenteditable="true"></div></div>');
+    createElement(e);
+    // CONTAINER.insertAdjacentHTML('beforeend', '<div class="element" draggable="true"><div class="element-content" contenteditable="true"></div></div>');
     // document.querySelector('.element:last-child').focus();
-    setTimeout(function(){
+    // setTimeout(function(){
       
-    CONTAINER.lastChild.focus()
-    }, 100)
+    // CONTAINER.lastChild.focus()
+    // }, 100)
     // e.target.insertAdjacentHTML('afterend', '<div class="element" contenteditable="true"></div>');
     // setTimeout(function(){CONTAINER.lastChild.focus()}, 500);
 
@@ -24,15 +24,21 @@ document.activeElement.addEventListener("keypress", function (e) {
 });
 
 
-function createElement() {
+function createElement(e) {
+  e.preventDefault();
   // añadimos nuevo parrafo
-  
-  // CONTAINER.insertAdjacentHTML('beforeend', '<div class="element" contenteditable="true"></div>');
-  // document.querySelector('.element:last-child').focus();
-  // CONTAINER.lastChild.focus()
+  var PARENT = e.target.parentElement;
+  PARENT.insertAdjacentHTML('afterend', '<div class="element" draggable="true"><div class="element-content" contenteditable="true"></div></div>');
+  PARENT.nextSibling.getElementsByClassName('element-content')[0].focus()
 
-  var p = document.createElement("div").setAttribute('contenteditable', 'true');
-  CONTAINER.append(p);
-  // CONTAINER.lastChild.focus()
+
+  // CONTAINER.insertAdjacentHTML('beforeend', '<div class="element" draggable="true"><div class="element-content" contenteditable="true"></div></div>');
+  // CONTAINER.lastChild.getElementsByClassName('element-content')[0].focus();
 
 }
+
+new Sortable(CONTAINER, {
+  group: 'content',
+  animation: 150,
+  ghostClass: 'dragging'
+});
