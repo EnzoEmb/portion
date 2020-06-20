7163b1e4daeb0fd694e202e12db839896a5be331
Enzo Vergara <enzoamb123@gmail.com>
2020-06-19 22:29:04 -0300

    sortable test - new grids at right

--- dist/portion.js
+++ dist/portion.js
@@ -1,6 +1,7 @@
 const BASE = document.getElementById("base")
 const CONTAINER = document.getElementById("content")
 const GRID_DROPZONE = document.getElementById("grid-dropzone")
+const MAX_GRIDS = 5;
 // const ELEMENT = document.querySelector(".element")
 
 // cuando presiona un boton en el elemento con foco
@@ -39,19 +40,45 @@ new Sortable(CONTAINER, {
   //   console.log(evt);
   // },
   onEnd: function (/**Event*/evt) {
-    console.log('ENDED');
-    console.log(evt.to);
-    if (evt.to.id == 'grid-dropzone') {
-      createContent();
-    }
-    console.log(evt.to.id);
+    //   console.log('ENDED');
+    // console.log(evt);
+    //   if (evt.to.id == 'grid-dropzone') {
+    //     createContent();
+    //   }
+    //   console.log(evt.to.id);
+    document.querySelector('.sortable-focused').classList.remove('sortable-focused')
   },
+  onMove: function (evt) {
+    // console.log(evt);
+
+    // console.log(evt);
+    // console.log(evt.from);
+    // console.log(evt.related);
+
+    if (evt.related != evt.from) {
+      evt.related.classList.add('sortable-focused')
+
+    } else {
+      evt.related.classList.remove('sortable-focused')
+
+    }
+  }
 });
 
 
 new Sortable(GRID_DROPZONE, {
   group: 'content',
-  animation: 150
+  animation: 150,
+  onAdd: function (/**Event*/evt) {
+    GRID_DROPZONE.classList.add('has-element');
+  },
+  onMove: function (evt) {
+    // console.log(evt);
+    // console.log(GRID_DROPZONE.getElementsByClassName('element').length);
+    if (GRID_DROPZONE.getElementsByClassName('element').length == 1) {
+      GRID_DROPZONE.classList.remove('has-element')
+    }
+  }
 });
 
 // onmousemove = function(e){console.log("mouse location:", e.clientX, e.clientY)}
