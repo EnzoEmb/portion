53a7d25d8191de6007b7ddf367d65959f5fc9549
Enzo Vergara <enzoamb123@gmail.com>
2020-06-20 21:25:20 -0300

    element icon

--- src/index.js
+++ src/index.js
@@ -1,8 +1,15 @@
 const BASE = document.getElementById("base")
 const CONTAINER = document.getElementById("content")
 const MAX_GRIDS = 5;
-const TEMPLATE_ELEMENT = `<div class="element"><div class="element-content" contenteditable="true"></div></div>`;
-const TEMPLATE_ROW = `<div class="row"><div class="column">` + TEMPLATE_ELEMENT + `</div></div>`;
+const TEMPLATE_ELEMENT = `
+<div class="element">
+  <div class="element-icon">a</div>
+  <div class="element-content" contenteditable="true"></div>
+</div>`;
+const TEMPLATE_ROW = `
+<div class="row">
+  <div class="column">` + TEMPLATE_ELEMENT + `</div>
+</div>`;
 
 
 
@@ -30,21 +37,20 @@ function createRow(e) {
 
   // añadimos nuevo row
   PARENT.insertAdjacentHTML('afterend', TEMPLATE_ROW);
-  PARENT.nextSibling.getElementsByClassName('element-content')[0].focus()
+  PARENT.nextElementSibling.getElementsByClassName('element-content')[0].focus()
 
   // iniciamos la columna
-  initColumn(PARENT.nextSibling.querySelector('.column'))
+  initColumn(PARENT.nextElementSibling.querySelector('.column'))
 
   // iniciamos el row
-  initRow(PARENT.nextSibling)
+  initRow(PARENT.nextElementSibling)
 
 }
 
 
 function createElement(e) {
-  console.log(e);
   e.target.parentElement.insertAdjacentHTML('afterend', TEMPLATE_ELEMENT);
-  e.target.parentElement.nextSibling.getElementsByClassName('element-content')[0].focus();
+  e.target.parentElement.nextElementSibling.getElementsByClassName('element-content')[0].focus();
 }
 
 
@@ -55,7 +61,7 @@ function initColumn(element) {
     draggable: '.element',
     onRemove(e) {
       // si la columna no tiene elementos, la eliminamos
-      if (e.from.childNodes.length === 0) {
+      if (e.from.querySelectorAll('.element').length === 0) {
         // si la row no tiene elementos la eliminamos
         if (e.from.parentElement.querySelectorAll('.element').length == 0) {
           e.from.parentElement.remove();
@@ -92,6 +98,9 @@ function initRow(element) {
 
 // iniciamos los elementos default
 function init() {
+
+  // añadimos la base inicial
+  document.getElementById('content').insertAdjacentHTML('beforeend', TEMPLATE_ROW);
 
   // iniciamos la primer columna
   initColumn(document.querySelector('.column'))
